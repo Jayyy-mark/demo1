e84c634e29f286b5c64de968fcf0adcc0701d7ca
htetzayar <htetzayar834@gmail.com>
2026-05-21 16:12:57 +0630

    update ui and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // 'sans' ဆိုတဲ့ မူလ font နေရာမှာ အစားထိုးလိုက်တာမို့ website တစ်ခုလုံး အလိုအလျောက် ပြောင်းသွားပါမယ်
-        sans: ['Inter', 'Noto Sans Myanmar', 'ui-sans-serif', 'system-ui'],
+        sans: ['Roboto', 'sans-serif'],
       },
     },
   },

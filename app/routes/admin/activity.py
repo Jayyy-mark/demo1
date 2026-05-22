68e39cab4c06110fd3eba7d95b1386aa247fda8e
Jayyy-mark <jaycoab2@gmail.com>
2026-05-22 11:32:17 +0630

    added decorators for login on routes

--- app/routes/admin/activity.py
+++ app/routes/admin/activity.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import admin_bp
+from app.https.auth.decorators import login_required
 
 @admin_bp.route("/activity", methods=['GET'], endpoint="activity")
+@login_required
 def index():
     return render_template("admin/activity.html", page="Activity")
 
--- app/routes/admin/user.py
+++ app/routes/admin/user.py
@@ -4,11 +4,13 @@
 
 from . import admin_bp
 from flask import render_template
+from app.https.auth.decorators import login_required
 
 #<!-- ==========================================
 #    USER ROUTES
 #=========================================== -->
 
 @admin_bp.route('/users', methods=['GET'], endpoint="users")
+@login_required
 def index():
         return render_template('admin/user.html', page="User Management")

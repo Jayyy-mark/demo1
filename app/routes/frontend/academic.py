650bc07560014d664a3d280f17365d9e4e2333a3
Jayyy-mark <jaycoab2@gmail.com>
2026-06-05 06:18:53 +0630

    added academic calendar on frontend

--- app/routes/frontend/academic.py
+++ app/routes/frontend/academic.py
@@ -4,3 +4,5 @@ from flask import render_template
 @frontend_bp.route('/academic', endpoint="academic")
 def academic():
     return render_template('frontend/academic.html')
+
+
--- app/routes/frontend/academic_calendar.py
+++ app/routes/frontend/academic_calendar.py
@@ -1,6 +1,6 @@
 from . import frontend_bp
 from flask import render_template
 
-@frontend_bp.route('/academic_cal', endpoint="academic_cal")
+@frontend_bp.route('/academic/calendar', endpoint="academic_calendar")
 def academic():
     return render_template('frontend/academic_calendar.html')

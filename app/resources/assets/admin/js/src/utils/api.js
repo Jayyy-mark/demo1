5450509ff74548e1a3aa8b9b71c8597748354caa
Jayyy-mark <jaycoab2@gmail.com>
2026-06-09 13:16:46 +0630

    updated on api.js

--- app/resources/assets/admin/js/src/utils/api.js
+++ app/resources/assets/admin/js/src/utils/api.js
@@ -1,7 +1,7 @@
 import { errorUtils } from "../error/error.utils.js";
 
 const api = axios.create({
-    baseURL: "https://ucstaungoo.up.railway.app/api",
+    baseURL: "https://ucstgo.up.railway.app/api",
     //baseURL: "http://127.0.0.1:5000/api",
     withCredentials: true
 });

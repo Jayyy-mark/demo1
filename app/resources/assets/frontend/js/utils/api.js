9241e340c69b33a182daf41beaf95d2131a5dd28
Jayyy-mark <jaycoab2@gmail.com>
2026-06-09 13:02:56 +0630

    updated on api.js

--- app/resources/assets/frontend/js/utils/api.js
+++ app/resources/assets/frontend/js/utils/api.js
@@ -1,4 +1,5 @@
 export const api = axios.create({
-    baseURL: "https://ucstaungoo.up.railway.app/api",
+    //baseURL: "https://ucstaungoo.up.railway.app/api",
+    baseURL: "https://ucstgo.up.railway.app/api",
     //baseURL: "http://127.0.0.1:5000/api",
 });
